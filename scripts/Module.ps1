e98aa52d61b95f3651c79209ceefe42212d764f3
Patrick El-Azem <p@elazem.com>
2024-04-16 09:59:08 -0400

    Start adding observability

---
+++ scripts/Module.ps1
@@ -0,0 +1,34 @@
+function Get-PlzmAzureModule()
+{
+  [CmdletBinding()]
+  param
+  (
+    [Parameter(Mandatory = $true)]
+    [object]
+    $UrlRoot
+  )
+
+  $moduleName = "plzm.Azure"
+  $localFolderPath = "./modules/$moduleName/"
+  $psm1FileName = "$moduleName.psm1"
+  $psd1FileName = "$moduleName.psd1"
+
+  if (!(Test-Path -Path $localFolderPath))
+  {
+    New-Item -Path $localFolderPath -ItemType "Directory" -Force
+  }
+
+  # PSM1 file
+  $url = ($UrlRoot + $psm1FileName)
+  Invoke-WebRequest -Uri "$url" -OutFile ($localFolderPath + $psm1FileName)
+
+  # PSD1 file
+  $url = ($UrlRoot + $psd1FileName)
+  Invoke-WebRequest -Uri "$url" -OutFile ($localFolderPath + $psd1FileName)
+
+  Import-Module "$localFolderPath" -Force
+
+  Write-Debug -Debug:$true -Message "Module $moduleName imported from $localFolderPath with version $((Get-Module $moduleName).Version)"
+
+  return $localFolderPath
+}
